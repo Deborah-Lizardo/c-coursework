546af24ecc635c87aa0a9c8d466c905e6afb8655
dehlizardo <deborah.lizardo@pucpr.edu.br>
2025-04-24 10:34:13 -0300

    Add new files

---
+++ Week_3_User_Input/factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+    int n;
+    int result = 0;
+    int factorial = 0;
+
+    printf("Please input the factorial number you want to calculate: \n");
+    scanf("%i", &n);
+
+    for (n != i; i ++, n--){
+        printf("factorial %i %i\n", n, i);
+        factorial = n - i ;
+        factorial = n * i; //
+        result = result+ factorial;
+        printf(The result is: %i\n", result);
+    }"
+    return 0;
+}
